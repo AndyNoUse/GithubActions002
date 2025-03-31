726a09b33b5a4c258f64f58888ac9f75bfcf7cc9
lonil <lonil005@gmail.com>
2025-03-31 11:52:12 +0200

    Initial commit

---
+++ src/main/java/user/User.java
@@ -0,0 +1,15 @@
+public class User {
+    //Skapa en testklass och utveckla efter TDD.
+    //
+    //Skapa ett test för en tänkt klass User autogenerera klassen ifrån testklassen
+    //
+    //Uppdatera testet så att konstruktorn i User får två parametrar av typen String
+    //Skriv koden som gör att testet i uppgift 3 blir godkänt och spara parametrarna i två attribut: userName, password. För alla kommande uppgifter gör likadant Red – Green – Refactor
+    //
+    //Skriv test som hämtar attributet userName
+    //
+    //Skriv test som hämtar attributet passWord
+    //
+    //Skriv ett test som ändrar userName
+    //
+}
