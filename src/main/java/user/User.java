29bcd9455bd35a13fe6cd601353ea36f76b3cb9c
lonil <lonil005@gmail.com>
2025-03-31 11:56:35 +0200

    test remove test

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,9 +1,21 @@
+package user;
+
 public class User {
-    //Skapa en testklass och utveckla efter TDD.
-    //
-    //Skapa ett test för en tänkt klass User autogenerera klassen ifrån testklassen
-    //
-    //Uppdatera testet så att konstruktorn i User får två parametrar av typen String
+    private String username, password;
+
+    User(String user,String password){
+        this.username = user;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     //Skriv koden som gör att testet i uppgift 3 blir godkänt och spara parametrarna i två attribut: userName, password. För alla kommande uppgifter gör likadant Red – Green – Refactor
     //
     //Skriv test som hämtar attributet userName
--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,4 +1,19 @@
 package user;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
+
+    @Test
+    public void addUser() {
+        User user = new User("Svensson", "Lösenord123");
+        assertEquals("Svensson", user.getUsername());
+        assertEquals("Lösenord123", user.getPassword());
+    }
+
+//    @Test
+//    public void changePassword() {
+//    }
 }
